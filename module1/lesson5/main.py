9ebed65a24e3db32de354186541282f7ce5190dd
SBwat <szymon.borys@wat.edu.pl>
2024-02-11 17:19:55 +0100

    Dodano - discount policy

---
+++ module1/lesson5/main.py
@@ -0,0 +1,65 @@
+from shop.apple import Apples
+from shop.potato import Potato
+from shop.order import Order, OrderElement
+from shop.product import Product
+from shop.discounts import regular_discount,christmas_discount
+
+
+def order_key(order):
+    return order._total_order()
+def class_example():
+    red = Apples('red', 'big', 5)
+    green = Apples('green', 'small', 3)
+    old_potato = Potato("old", "small", 2)
+    young_potato = Potato("young", "big", 10)
+    print(repr(red))
+    print(str(red))
+
+    product1 = Product("Woda", "Jedzenie", 10)
+    product2 = Product("wóda", "płyny", 20)
+    product3 = Product("woda", "płyny", 10)
+    print(product1 == product2)
+    print(product1 == product3)
+    print(product1 == red)
+
+    oe1 = OrderElement(product1, 10)
+    oe2 = OrderElement(product1, 5)
+    oe3 = OrderElement(product2, 10)
+    oe4 = oe1
+    print(oe1 == oe2)
+    print(oe1 == oe3)
+    print(oe1 == oe4)
+    print(str(oe1))
+
+    order3 = Order.random_order_generator("Jacek")
+    order4 = Order("Marek", [oe1, oe2])
+    order5 = Order("Jacek", [oe1, oe2], discount = christmas_discount)
+    order6 = Order("Marek", [oe2, oe1], discount = regular_discount)
+
+    print(str(order3))
+    print(f"Długość zamówienia: {len(order3)}")
+    order3.add_product(product1, 8)
+    print(str(order3))
+    print(f"Długość zamówienia: {len(order3)}")
+
+    print(f"Porównanie 4 z 5: {order4 == order5}")
+    print(f"Porównanie 4 z 6: {order4 == order6}")
+
+    print(str(order4))
+    print(str(order5))
+    print(str(order6))
+
+    orders_to_sort = []
+    for i in range(5):
+        new_order = Order.random_order_generator("Customer")
+        print(f"{i} -> {new_order.total_price}")
+        orders_to_sort.append(new_order)
+
+    orders_to_sort.sort(key=order_key)
+    for order in orders_to_sort:
+        print(order.total_price)
+
+
+
+if __name__ == '__main__':
+    class_example()
---
+++ module1/lesson5/shop/order.py
@@ -0,0 +1,92 @@
+import random
+from shop.product import Product
+from shop.tax_calculator import Tax
+from shop.discounts import no_discount
+
+
+class OrderElement:
+    def __init__(self, product, quantity):
+        self.product = product
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"{self.product}, \t Ilość: {self.quantity}"
+
+    def __eq__(self, other):
+        if self.__class__ != other.__class__:
+            return NotImplemented
+        return self.quantity == other.quantity and self.product == other.product
+
+    def total_price(self):
+        return self.product.price * self.quantity
+
+
+class Order:
+    MAX_ORDER_ELEMENTS = 20
+
+    def __init__(self, name, shopping_list=None, discount=None):
+        self.name = name
+        if shopping_list is None:
+            shopping_list = []
+        if len(shopping_list) > Order.MAX_ORDER_ELEMENTS:
+            self._list = shopping_list[:Order.MAX_ORDER_ELEMENTS]
+        else:
+            self._list = shopping_list
+
+        if discount is None:
+            discount = no_discount
+        self.discount = discount
+        self.total_price = self._total_order()
+
+    def __str__(self):
+        total_tax = 0
+        sepa = "=" * 40
+        products = "\n"
+        for prod in self._list:
+            tax = Tax.calculate(prod.product.category, prod.total_price())
+            products += f"\t {prod} \t\t Podatek: {tax: .2f} zł.\n"
+            total_tax += tax
+        return (f"{sepa}\nZamawiający: {self.name}\nKwota całkowita zamówienia: {self._total_order()} zł.{products}\n"
+                f"Podatek do zapłacenia: {total_tax: .2f} zł. \n"
+                f"Całkowita kwota do zapłaty: {self._total_order()+total_tax: .2f} zł. \n{sepa}")
+
+    def __len__(self):
+        return len(self._list)
+
+    def __eq__(self, other):
+        if self.__class__ != other.__class__:
+            return NotImplemented
+        if self.name == other.name and len(self) == len(other):
+            for product in self._list:
+                if product not in other._list:
+                    return False
+            return True
+        else:
+            return False
+
+    def _total_order(self):
+        total_price = 0
+        for i in self._list:
+            total_price += OrderElement.total_price(i)
+        return self.discount(total_price)
+
+    def add_product(self, product, quantity):
+        if len(self._list) >= Order.MAX_ORDER_ELEMENTS:
+            print(f"Przekroczono limit produktów, nowy produkt nie został dodany!")
+        else:
+            self._list.append(OrderElement(product, quantity))
+            self.total_price = self._total_order()
+
+    @classmethod
+    def random_order_generator(cls, name):
+        new_list = []
+        size = random.randint(1, cls.MAX_ORDER_ELEMENTS)
+        for i in range(size):
+            prod_name = "Produkt" + str(i)
+            prod_cat = "Kategoria" + str(random.randint(1, 9))
+            prod_price = random.randint(1, 20)
+            quantity = random.randint(1, 50)
+            new_list.append(OrderElement(Product(prod_name, prod_cat, prod_price), quantity))
+
+        order = Order(name, new_list)
+        return order
